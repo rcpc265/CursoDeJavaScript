3ec698c86fce0ce77fba958e7fb0f91f0707bcf9
rcpc265 <cangahaualapinedarafael@gmail.com>
2022-09-02 19:51:59 -0700

    minor changes

--- tercer-ejercicio-con-node/factorial-for.js
+++ tercer-ejercicio-con-node/factorial-for.js
@@ -1,7 +1,7 @@
 let number = 10
 let factorial = 1;
 
-for (let i = 2; i <= number; i++) {
+for (let i = 1; i <= number; i++) {
   factorial *= i;
 }
 
--- tercer-ejercicio-con-node/factorial-while.js
+++ tercer-ejercicio-con-node/factorial-while.js
@@ -1,5 +1,6 @@
-let number = 10
+let number = 10;
 let factorial = 1;
+
 while (number > 1) {
   factorial *= number;
   number--;

b942c83da43f617594145c280b5689bef3198baa
rcpc265 <cangahaualapinedarafael@gmail.com>
2022-09-03 20:56:55 -0700

    Add more exercises

---
+++ 05-numeros/index.js
@@ -0,0 +1,11 @@
+const heightInCentimeters = 180;
+const heightInMeters = 1.85;
+
+const weightInKilograms = 70.3;
+
+const roundedHeight = Math.ceil(heightInMeters);
+
+const roundedWeight = Math.floor(weightInKilograms);
+
+const isEqual = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log(isEqual);
